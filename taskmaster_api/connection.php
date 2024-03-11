7d3387bea633d61898b0e11bb9b98bac1835a149
MarePet <74553764+MarePet@users.noreply.github.com>
2024-03-11 15:41:50 +0100

    Add files via upload

---
+++ taskmaster_api/connection.php
@@ -0,0 +1,8 @@
+<?php 
+$serverHost = "sql207.infinityfree.com";
+$user = "if0_36141427";
+$password = "I9VcqnOoYVB";
+$databse = "if0_36141427_taskmaster_app";
+
+$connectNow = new mysqli($serverHost, $user, $password, $databse);
+
